4ebd4d9d3d4c4024acc4027cc76c9a88a29b65b5
arch lw2333 <2374046775@qq.com>
2025-03-29 10:57:21 +0000

    use std::atomic for task allocate: 3.x speedup

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+#include <atomic>
 #include <thread>
 
 #include "CycleTimer.h"
 #include "mandel.h"
+
+std::atomic<int> nextTask(0);
 
 typedef struct {
   float x0, x1;
@@ -13,8 +16,8 @@ typedef struct {
   int maxIterations;
   int* output;
   int threadId;
-  int numThreads;
-  float time;
+  int totalTasks;
+  float* time;
 } WorkerArgs;
 
 extern void mandelbrotSerial(float x0, float y0, float x1, float y1, int width,
@@ -33,24 +36,34 @@ void workerThreadStart(WorkerArgs* const args) {
   // half of the image and thread 1 could compute the bottom half.
   double startTime = CycleTimer::currentSeconds();
 
-  const float dx = (args->x1 - args->x0) / args->width;
-  const float dy = (args->y1 - args->y0) / args->height;
+  while (true) {
+    // get next task and add by 1
+    int taskId = nextTask.fetch_add(1);
+    if (taskId >= args->totalTasks) break;
 
-  const int startRow = args->threadId * args->height / args->numThreads;
-  const int endRow = (args->threadId + 1) * args->height / args->numThreads;
+    const float dx = (args->x1 - args->x0) / args->width;
+    const float dy = (args->y1 - args->y0) / args->height;
 
-  for (int j = startRow; j < endRow; j++) {
-    for (unsigned int i = 0; i < args->width; ++i) {
-      const float x = args->x0 + i * dx;
-      const float y = args->y0 + j * dy;
+    // now the start and end row must change.
+    const int startRow = taskId * args->height / args->totalTasks;
+    const int endRow = (taskId + 1) * args->height / args->totalTasks;
+    if (endRow > args->height) {
+      break;
+    }
 
-      const int index = (j * args->width + i);
-      args->output[index] = mandel(x, y, args->maxIterations);
+    for (int j = startRow; j < endRow; j++) {
+      for (unsigned int i = 0; i < args->width; ++i) {
+        const float x = args->x0 + i * dx;
+        const float y = args->y0 + j * dy;
+
+        const int index = (j * args->width + i);
+        args->output[index] = mandel(x, y, args->maxIterations);
+      }
     }
   }
 
   double endTime = CycleTimer::currentSeconds();
-  args->time = endTime - startTime;
+  args->time[args->threadId] += endTime - startTime;
 }
 
 //
@@ -61,6 +74,12 @@ void workerThreadStart(WorkerArgs* const args) {
 void mandelbrotThread(int numThreads, float x0, float y0, float x1, float y1,
                       int width, int height, int maxIterations, int output[],
                       float time[]) {
+  // observing that the max computation time is four times as bigger as the edge
+  // case computation time, when devided by the number of threads
+  // so we can part the computation by height/(4*numThreads)
+  // and build a pool for threads to compute
+  nextTask.store(0, std::memory_order_relaxed);
+
   static constexpr int MAX_THREADS = 32;
 
   if (numThreads > MAX_THREADS) {
@@ -83,9 +102,9 @@ void mandelbrotThread(int numThreads, float x0, float y0, float x1, float y1,
     args[i].width = width;
     args[i].height = height;
     args[i].maxIterations = maxIterations;
-    args[i].numThreads = numThreads;
     args[i].output = output;
-    args[i].time = 0.f;
+    args[i].time = time;
+    args[i].totalTasks = numThreads * 4;
     args[i].threadId = i;
   }
 
@@ -102,7 +121,5 @@ void mandelbrotThread(int numThreads, float x0, float y0, float x1, float y1,
   // join worker threads
   for (int i = 1; i < numThreads; i++) {
     workers[i].join();
-    time[i] += args[i].time;
   }
-  time[0] += args[0].time;
 }
