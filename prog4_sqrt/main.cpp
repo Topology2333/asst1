0561a490ece50352307145e7a1aebb5d6a043088
arch lw2333 <2374046775@qq.com>
2025-04-11 02:19:47 +0000

    p4: gen best and worst cases

--- prog4_sqrt/main.cpp
+++ prog4_sqrt/main.cpp
@@ -33,7 +33,13 @@ int main() {
     // to you generate best and worse-case speedups
 
     // starter code populates array with random input values
-    values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+    // values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+
+		// worst: all 1.0f
+    // values[i] = 1.0f;
+
+		// best: all 2.999f
+    values[i] = 2.999f;
   }
 
   // generate a gold version to check results
